9e1acee2db594ce5e444fd22077a8fab693afa80
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 12:03:34 +0700

    Menambahkan Object

--- src/rumahmakan/MainMenu.java
+++ src/rumahmakan/MainMenu.java
@@ -2,6 +2,14 @@ package rumahmakan;
 
 public class MainMenu {
     public static void main(String[] args) {
-        
+        //objek
+        MenuItem[] daftarMenu = {
+            new MenuMakanan("Nasi Goreng", 15000, "Utama"),
+            new MenuMakanan("Nasi Putih", 5000, "Utama"),
+            new MenuMakanan("Ayam Bakar", 18000, "Lauk"),
+            new MenuMakanan("Tempe Orek", 8000, "Pelengkap"),
+            new MenuMinuman("Teh Manis", 5000, true),
+            new MenuMinuman("Kopi Hitam", 7000, false)
+        };
     }
 }
