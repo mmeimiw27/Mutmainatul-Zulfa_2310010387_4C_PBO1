841d0c5ae80da77926bd2f064da7cf482b1d9f8b
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 11:58:45 +0700

    Menambahkan IO Sederhana dan Eror Handling

--- src/rumahmakan/InputMenu.java
+++ src/rumahmakan/InputMenu.java
@@ -4,4 +4,19 @@ import java.util.Scanner;
 
 public class InputMenu {
     private static Scanner sc = new Scanner(System.in);
+    public static int inputInt(String pesan) {
+        while (true) {
+            try {
+                System.out.print(pesan);
+                return Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Input harus angka!");
+            }
+        }
+    }
+    
+    public static String inputString(String pesan) {
+        System.out.print(pesan);
+        return sc.nextLine();
+    }
 }
