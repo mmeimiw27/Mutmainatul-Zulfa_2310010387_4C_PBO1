6963ff9262b0198873f77afcd44dca43635502ad
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 11:30:02 +0700

    Menambahkan method cetak struk

--- src/rumahmakan/Kasir.java
+++ src/rumahmakan/Kasir.java
@@ -8,7 +8,7 @@ public class Kasir {
     private ArrayList<MenuItem> daftarPesanan = new ArrayList<>();
     
     public void tambahPesanan(MenuItem item) {
-        // Materi 10: Seleksi → cek stok sebelum menambahkan pesanan
+        //Seleksi: cek stok sebelum menambahkan pesanan
         if (item.getStok() > 0) {
             item.kurangiStok(); // stok dikurangi saat dipesan
             daftarPesanan.add(item);
@@ -16,5 +16,17 @@ public class Kasir {
         } else {
             System.out.println("Maaf, " + item.getNama() + " sedang habis.");
         }
+    }
+    public void cetakStruk() {
+        System.out.println("\n== STRUK PESANAN ==");
+        double total = 0;
+
+        // Perulangan
+        for (MenuItem item : daftarPesanan) {
+            System.out.println("- " + item.getNama() + ": Rp" + item.getHarga());
+            total += item.getHarga();
+        }
+
+        System.out.println("TOTAL: Rp" + total);
     }
 }
