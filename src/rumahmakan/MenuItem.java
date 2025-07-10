3666aa85b9e97be250d60a29c4a771a3b6637a2f
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 08:55:52 +0700

    Membuat Constuctor untuk menginisialisasi atribut

--- src/rumahmakan/MenuItem.java
+++ src/rumahmakan/MenuItem.java
@@ -2,8 +2,15 @@ package rumahmakan;
 
 //Class
 public class MenuItem {
-    //Atribute
+    //Atribute (encapsulation : Modifier Private)
     private String nama;
     private double harga;
     private int stok;
+    
+    //Constructor
+    public MenuItem(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = 1; 
+    }
 }
