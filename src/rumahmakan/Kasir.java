bd0fa86d9f0941635bec5c6d1311f47a792266c3
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 11:25:58 +0700

    Membuat list dan membuat method

--- src/rumahmakan/Kasir.java
+++ src/rumahmakan/Kasir.java
@@ -4,5 +4,17 @@ import java.util.ArrayList;
 //Mengimpor kelas ArrayList
 
 public class Kasir {
+    //membuat list untuk menyimpan objek menuItem
+    private ArrayList<MenuItem> daftarPesanan = new ArrayList<>();
     
+    public void tambahPesanan(MenuItem item) {
+        // Materi 10: Seleksi → cek stok sebelum menambahkan pesanan
+        if (item.getStok() > 0) {
+            item.kurangiStok(); // stok dikurangi saat dipesan
+            daftarPesanan.add(item);
+            System.out.println(item.getNama() + " ditambahkan ke pesanan.");
+        } else {
+            System.out.println("Maaf, " + item.getNama() + " sedang habis.");
+        }
+    }
 }
