7cfc7d828f6091cfa9d8acf148084b12c192725c
mmeimiw27 <mutmainatulzulfaaa@gmail.com>
2025-07-10 09:21:41 +0700

    Menambahkan Inheritance & memanggil constructor superclass

--- src/rumahmakan/MenuMakanan.java
+++ src/rumahmakan/MenuMakanan.java
@@ -1,5 +1,12 @@
 package rumahmakan;
 
-public class MenuMakanan {
+//Inheritance : MenuMakanan mewarisi dari MenuItem
+public class MenuMakanan extends MenuItem {
+    private String jenis;
+    
+    public MenuMakanan(String nama, double harga, String jenis) {
+        super(nama, harga); 
+        this.jenis = jenis;
+    }
     
 }
